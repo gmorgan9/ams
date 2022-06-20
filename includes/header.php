0a53daf626a153905e7d8d76ef16124ef418a4f1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 15:20:45 -0600

    update

--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,50 @@
 <!--Main Navigation-->
 <header>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href="#">Logo</a>
 
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 1</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 2</a>
-    </li>
+<!-- Navbar -->
+<nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+      aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
 
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Dropdown link
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Link 1</a>
-        <a class="dropdown-item" href="#">Link 2</a>
-        <a class="dropdown-item" href="#">Link 3</a>
-      </div>
-    </li>
-  </ul>
+<!-- Brand -->
+<a class="navbar-brand" href="#">
+  <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="25" alt="MDB Logo"
+    loading="lazy" />
+</a>
+<!-- Search form -->
+<form class="d-none d-md-flex input-group w-auto my-auto">
+  <input autocomplete="off" type="search" class="form-control rounded"
+    placeholder='Search (ctrl + "/" to focus)' style="min-width: 225px;" />
+  <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
+</form>
+
+<!-- Right links -->
+<ul class="navbar-nav ms-auto d-flex flex-row">
+<li class="nav-item">
+  <a class="nav-link me-3 me-lg-0" href="#">
+    <i class="fas fa-fill-drip"></i>
+  </a>
+</li>
+<!-- Icon -->
+<li class="nav-item me-3 me-lg-0">
+  <a class="nav-link" href="#">
+    <i class="fab fa-github"></i>
+  </a>
+</li>
+</ul>
+</div>
+<!-- Container wrapper -->
 </nav>
-  <!-- Navbar -->
+<!-- Navbar -->
 </header>
+<!--Main Navigation-->
+<!--Main layout-->
 <main style="margin-top: 58px;">
   <div class="container pt-4"></div>
 </main>
+<!--Main layout-->
