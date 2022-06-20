578d783455053312e7bd98832acc0c2cbe1234ae
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 16:59:51 -0600

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
               <div class="position-sticky">
                 <div class="list-group list-group-flush mx-3 mt-4">
-                  <a href="/pages/dashboard.php" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                  <a href="/" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
                     <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>  Dashboard</span>
                   </a>
                   <a href="/pages/classes.php" class="list-group-item list-group-item-action py-2 ripple">
