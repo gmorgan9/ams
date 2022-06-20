86a2c8ec519f16e4f91fb3473f30ff5ef479dc22
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 13:43:14 -0600

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,9 +1,8 @@
 <div class="sidebar">
-<h2 class="logo">
-    <img src="assets/images/logo1.png" alt="">
-</h2>
-
-
-
-
+<img class="logo" src="assets/images/logo1.png" alt="">
+    <ul>
+        <li><a href="/admin/posts/index.php">Manage Posts</a></li>
+        <li><a href="/admin/users/index.php">Manage Users</a></li>
+        <li><a href="/admin/topics/index.php">Manage Topics</a></li>
+    </ul>
 </div>
