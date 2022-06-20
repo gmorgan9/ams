b9213503300d239d7ea0ec8f1090a44958a14991
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 14:47:57 -0600

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,27 @@
-<div class="left-sidebar">
+<!-- <div class="left-sidebar">
     <ul>
         <li><a href="/admin/posts/index.php">Manage Posts</a></li>
         <li><a href="/admin/users/index.php">Manage Users</a></li>
         <li><a href="/admin/topics/index.php">Manage Topics</a></li>
     </ul>
-</div>
+</div> -->
+
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+    <div class="position-sticky">
+      <div class="list-group list-group-flush mx-3 mt-4">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action py-2 ripple"
+          aria-current="true"
+        >
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
+          <i class="fas fa-chart-area fa-fw me-3"></i><span>Webiste traffic</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-lock fa-fw me-3"></i><span>Password</span></a
+        >
+      </div>
+    </div>
+  </nav>
