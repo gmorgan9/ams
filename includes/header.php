ba207c7cebbabf5af9a4f800300fa095b25ac7cf
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 15:24:45 -0600

    update

--- includes/header.php
+++ includes/header.php
@@ -5,28 +5,17 @@
 <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
   <!-- Container wrapper -->
   <div class="container-fluid">
-    <!-- Toggle button -->
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
-      aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
 
 <!-- Brand -->
 <a class="navbar-brand" href="#">
   <h3>Assignment Management System</h3>
 </a>
-<!-- Search form -->
-<form class="d-none d-md-flex input-group w-auto my-auto">
-  <input autocomplete="off" type="search" class="form-control rounded"
-    placeholder='Search (ctrl + "/" to focus)' style="min-width: 225px;" />
-  <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
-</form>
 
 <!-- Right links -->
   <ul class="navbar-nav ms-auto d-flex flex-row">
     <li class="nav-item">
       <a class="nav-link me-3 me-lg-0" href="#">
-        <i class="fas fa-user"></i><p>Profile</p>
+        <i class="fas fa-user"></i>
       </a>
     </li>
   </ul>
