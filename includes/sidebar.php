e8b246675ffb13d3a435fda31f732613a7ceb27f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 16:58:23 -0600

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,13 +1,13 @@
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
               <div class="position-sticky">
                 <div class="list-group list-group-flush mx-3 mt-4">
-                  <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                  <a href="/pages/dashboard.php" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
                     <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>  Dashboard</span>
                   </a>
-                  <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                  <a href="/pages/classes.php" class="list-group-item list-group-item-action py-2 ripple">
                   <i class="fa-solid fa-landmark"></i><span>  Classes</span>
                   </a>
-                  <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-clipboard-list"></i><span>  Assignments</span></a>
+                  <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-clipboard-list"></i><span>  Assignments</span></a>
                 </div>
               </div>
             </nav>
