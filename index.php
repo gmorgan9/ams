a09d86bf44f331df1042da14dff92b6f57611ed4
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 14:33:45 -0600

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include("path.php");
     <!-- <link rel="icon" type="image/x-icon" href="assets/img/bell-regular.svg"> -->
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="assets/css/style.css?v=0.14">
+    <link rel="stylesheet" href="assets/css/style.css?v=0.15">
 
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
