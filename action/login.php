cf80ea6cb32b5f937aad6663765a2f039a02bb1c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-20 18:07:59 -0600

    update

--- action/login.php
+++ action/login.php
@@ -20,101 +20,122 @@
     <title>Login - AMS</title>
 
     <style>
-        body {
-          background: #ABCDEF;
-          font-family: Assistant, sans-serif;
-          display: flex;
-          min-height: 90vh;
-        }
-        .login {
-    color: white;
-    background: #136a8a;
-    background: 
-      -webkit-linear-gradient(to right, #267871, #136a8a);
-    background: 
-      linear-gradient(to right, #267871, #136a8a);
-    margin: auto;
-    box-shadow: 
-      0px 2px 10px rgba(0,0,0,0.2), 
-      0px 10px 20px rgba(0,0,0,0.3), 
-      0px 30px 60px 1px rgba(0,0,0,0.5);
-    border-radius: 8px;
-    padding: 50px;
-  }
-  .login .head {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .login .head .title {
-    font-size: 2.2em;
-  }
-  .login .msg {
-    text-align: center;
-  }
-  .login .form input[type=text].text {
-    border: none;
-    background: none;
-    box-shadow: 0px 2px 0px 0px white;
-    width: 100%;
-    color: white;
-    font-size: 1em;
-    outline: none;
-  }
-  .login .form .text::placeholder {
-    color: #D3D3D3;
-  }
-  .login .form input[type=password].password {
-    border: none;
-    background: none;
-    box-shadow: 0px 2px 0px 0px white;
-    width: 100%;
-    color: white;
-    font-size: 1em;
-    outline: none;
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-  .login .form .password::placeholder {
-    color: #D3D3D3;
-  }
-  .login .form .btn-login {
-    background: none;
-    text-decoration: none;
-    color: white;
-    box-shadow: 0px 0px 0px 2px white;
-    border-radius: 3px;
-    padding: 5px 2em;
-    transition: 0.5s;
-  }
-  .login .form .btn-login:hover {
-    background: white;
-    color: dimgray;
-    transition: 0.5s;
-  }
-  .login .forgot {
-    text-decoration: none;
-    color: white;
-    float: right;
-  }
+        * {
+	margin: 0px; 
+	padding: 0px; 
+	box-sizing: border-box;
+}
+
+body, html {
+	height: 100%;
+	font-family: Raleway-Regular, sans-serif;
+}
+
+/*---------------------------------------------*/
+a {
+	font-family: Raleway-Regular;
+	font-size: 14px;
+	line-height: 1.7;
+	color: #666666;
+	margin: 0px;
+	transition: all 0.4s;
+	-webkit-transition: all 0.4s;
+  -o-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+}
+
+a:focus {
+	outline: none !important;
+}
+
+a:hover {
+	text-decoration: none;
+  color: #57b846;
+}
+
+/*---------------------------------------------*/
+h1,h2,h3,h4,h5,h6 {
+	margin: 0px;
+}
+
+p {
+	font-family: Raleway-Regular;
+	font-size: 14px;
+	line-height: 1.7;
+	color: #666666;
+	margin: 0px;
+}
+
+ul, li {
+	margin: 0px;
+	list-style-type: none;
+}
+
+
+/*---------------------------------------------*/
+input {
+	outline: none;
+	border: none;
+}
+
+textarea {
+  outline: none;
+  border: none;
+}
+
+textarea:focus, input:focus {
+  border-color: transparent !important;
+}
     </style>
 </head>
 <body>
 
-<section class="login" id="login">
-  <div class="head">
-  <h1 class="title">Universe Explorer</h1>
-  </div>
-  <p class="msg">Welcome back</p>
-  <div class="form">
-    <form action="login.php" method="post">
-  <input type="text" placeholder="Enter Username..." class="text" id="username" required><br>
-  <input type="password" placeholder="Enter Password..." class="password"><br>
-  <a href="#" class="btn-login" id="do-login">Login</a>
-  <a href="#" class="forgot">Forgot?</a>
-    </form>
-  </div>
-</section>
+<div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+                <form class="login100-form validate-form flex-sb flex-w">
+                    <span class="login100-form-title p-b-32">
+                    Account Login
+                    </span>
+                    <span class="txt1 p-b-11">
+                    Username
+                    </span>
+                    <div class="wrap-input100 validate-input m-b-36" data-validate="Username is required">
+                        <input class="input100" type="text" name="username">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <span class="txt1 p-b-11">
+                    Password
+                    </span>
+                    <div class="wrap-input100 validate-input m-b-12" data-validate="Password is required">
+                        <span class="btn-show-pass">
+                            <i class="fa fa-eye"></i>
+                        </span>
+                        <input class="input100" type="password" name="pass">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <div class="flex-sb-m w-full p-b-48">
+                        <div class="contact100-form-checkbox">
+                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                            <label class="label-checkbox100" for="ckb1">
+                            Remember me
+                            </label>
+                        </div>
+                        <div>
+                            <a href="#" class="txt3">
+                            Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                        Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
