9764838ab81418358423929ebf6a55c06e05634b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 08:15:02 -0600

    update

--- action/register.php
+++ action/register.php
@@ -101,6 +101,7 @@ if(isset($_POST['login_user'])) {
         <input name="password" class="form-control" placeholder="Enter password" type="password" id="password">
         
     </div>
+    </div> <!-- form-group// -->
     <div class="form-group input-group w-75">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
@@ -108,6 +109,7 @@ if(isset($_POST['login_user'])) {
         <input name="confpassword" class="form-control" placeholder="Enter password" type="password" id="confpassword">
         
     </div>
+    </div> <!-- form-group// -->
 
     </div> 
     <input style="margin-left: 65px;" type="checkbox" onclick="myFunction()"> Show Password<!-- form-group// -->   
