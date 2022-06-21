1f91299c90732c2021487e6dda6f2f810a6ca76c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 10:47:33 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -58,7 +58,7 @@ if(!isset($_SESSION['username'])){
 
 
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title"><?php echo $_SESSION['username']; ?></h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
