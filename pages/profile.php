98977b574a94756671032883fae2dd9cb3b0206e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 11:47:12 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -40,7 +40,7 @@ if(!isset($_SESSION['username'])){
 <?php //include(ROOT_PATH . "/includes/sidebar.php"); ?>
 
 <div class="main">
-<button style="float: right; margin-Left:-80px;" type="button" class="btn btn-secondary">Secondary</button>
+<button style="float: right; margin-Left:-100px;" type="button" class="btn btn-secondary">Secondary</button>
     <h3 class="page_title">Profile</h3>
 </div>
 <div class="d-flex justify-content-center">
