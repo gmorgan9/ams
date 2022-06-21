b8208da9bf46ce4918e1a037ab7a067108bb5d06
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 10:42:35 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -49,7 +49,7 @@ if(!isset($_SESSION['username'])){
             <div class="bg-circle">
                 <div class="sm-circle">
                     <div class="d-flex justify-content-center">
-                        <img class="user-header" style="height:100px; border-radius:100px;" src="../assets/images/image.JPG" alt="">
+                        <img class="user-header" style="height:100px; border-radius:150px;" src="../assets/images/image.JPG" alt="">
                         <!-- <i class="user-header fa-solid fa-user fa-3x"></i> -->
                     </div>
                 </div>
