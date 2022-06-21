115d604d2582552912cf389d705a75d8301c64ce
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 12:34:48 -0600

    udpate

--- action/courses/add-course.php
+++ action/courses/add-course.php
@@ -13,18 +13,18 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Font Awesome -->
-    <link href="../assets/fontawesome/css/all.css" rel="stylesheet">
+    <link href="../../assets/fontawesome/css/all.css" rel="stylesheet">
 
     <!-- Favicon -->
     <!-- <link rel="icon" type="image/x-icon" href="assets/img/bell-regular.svg"> -->
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="../assets/css/style.css?v=2.2">
+    <link rel="stylesheet" href="../../assets/css/style.css?v=2.2">
 
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Register - AMS</title>
+    <title>Add Course - AMS</title>
 </head>
 <body>
 
@@ -33,7 +33,7 @@ session_start();
 
 
 <div class="d-flex justify-content-center">
-<form action="register.php" class="log-form" method="post">
+<form action="add-course.php" class="log-form" method="post">
 <?php //include('errors.php'); ?>
 
 <div class="form-header d-flex justify-content-center">
