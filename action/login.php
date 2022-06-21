8a135e4aae08f3396fe3e87d26c4611e25736b74
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 09:11:48 -0600

    update

--- action/login.php
+++ action/login.php
@@ -75,7 +75,9 @@ session_start();
 <br>
     <p class="d-flex justify-content-center">
         <span>Forgot Password? <button type="submit" name="reset_btn">Reset</button></span>
-        <br><br>
+        <br>
+</p>
+        <p class="d-flex justify-content-center">
 		<span>Not yet a member? <a href="register.php">Sign up</a></span>
 	</p>                                                          
 </form>
