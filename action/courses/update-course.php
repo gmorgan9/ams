ba13f9676e4d7b592ba352f9d453a708ff26c29c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 16:47:14 -0600

    update

--- action/courses/update-course.php
+++ action/courses/update-course.php
@@ -6,7 +6,117 @@ session_start();
 
 <?php
 
-   // Define variables and initialize with empty values
+
+// Define variables and initialize with empty values
+$course = "";
+$course_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST['update_course'])){
+    // Get hidden input value
+    $course_id = $_POST["course_id"];
+    
+    // Validate address address
+    $input_course = trim($_POST["course"]);
+    if(empty($input_course)){
+        $course_err = "Please enter a course.";     
+    } else{
+        $course = $input_course;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($course_err)){
+        // Prepare an update statement
+        $sql = "UPDATE course SET course=? WHERE course_id=?";
+         
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_course, $param_course_id);
+            
+            // Set parameters
+            $param_course = $course;
+            $param_course_id = $course_id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: /");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["cid"]) && !empty(trim($_GET["cid"]))){
+        // Get URL parameter
+        $cid =  trim($_GET["cid"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM course WHERE course_id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $cid;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $course = $row["course"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: die-page1.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($conn);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: die-page1.php");
+        exit();
+    }
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- // Define variables and initialize with empty values
    $course = $title = $taken = $day = $time = $location = $section = $instructor = $credits = $mode = "";
 
    $course_err = $title_err = $taken_err = $day_err = $time_err = $location_err = $section_err = $instructor_err = $credits_err = $mode_err = "";
@@ -225,7 +335,7 @@ if(isset($_POST["update_course"])){
 }
 
 
-?>
+?> -->
 
 
 <!DOCTYPE html>
