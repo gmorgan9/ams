85c30f60b345390221e697f14c48483857f6b494
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 07:38:48 -0600

    update

--- database/connection.php
+++ database/connection.php
@@ -5,7 +5,7 @@ $password = "Morgan22!";
 $database = "ams";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password. $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
