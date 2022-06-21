f90f2e89eab6fbe017101d75d12ed4adf976ce95
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 13:56:42 -0600

    update

--- pages/courses.php
+++ pages/courses.php
@@ -50,7 +50,7 @@ if(!isset($_SESSION['username'])){
       }
       .sem-btn {
         float: left;
-        margin-right: -180px;
+        margin-right: -190px;
         margin-left: 15px;
       }
       .page_subtitle {
