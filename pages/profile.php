a8be5854dc2b65fc8ecaed4ff5b8ba6867ff2280
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 10:50:05 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -57,9 +57,9 @@ if(!isset($_SESSION['username'])){
     
 
 
-      <div class="card-body d-flex justify-content-center">
-        <h5 class="card-title"><?php echo $_SESSION['username']; ?></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="card-body">
+        <h5 class="card-title text-center"><?php echo $_SESSION['username']; ?></h5>
+        <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
