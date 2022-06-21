2cd9e0ddc94dfd315fdcf52cd53341bc2352589c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 09:28:07 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -42,13 +42,17 @@ if(!isset($_SESSION['username'])){
 <div class="main">
     <h3 class="page_title">Profile</h3>
 </div>
-<div class="card" style="width: 35rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+<div class="d-flex justify-content-center">
+    
+    <div class="card" style="width: 35rem;">
+      <img class="card-img-top" src="..." alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
 </div>
 
 
