f46fd956584bd9d636022889f4866d7b5f72176f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 17:37:17 -0600

    update

--- pages/assignment-update.php
+++ pages/assignment-update.php
@@ -1,12 +1,185 @@
 <?php 
-if(isset($_POST['add-score'])) {
-$aid = $_GET['uid'];
-$query = "SELECT score FROM assignments WHERE id='$aid'";
-$result = mysqli_query( $conn, $query );
-$row = mysqli_fetch_assoc( $query );
+include("../path.php");
+include("../database/functions.php");
+session_start();
 
-$newhp = $row['score'];
-$query = "UPDATE assignments SET score= ? WHERE id='$aid'";
-$result = mysqli_query( $conn, $query );
+
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["add-score"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate address address
+    $input_score = trim($_POST["score"]);
+    if(empty($input_score)){
+        $score_err = "Please enter a Score.";     
+    } else{
+        $score = $input_score;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($score_err)){
+        // Prepare an update statement
+        $sql = "UPDATE assignments SET score=? WHERE id=?";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ii", $param_score, $param_id);
+            
+            // Set parameters
+            $param_score = $score;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["iud"]) && !empty(trim($_GET["uid"]))){
+        // Get URL parameter
+        $id =  trim($_GET["uid"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM assignments WHERE id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $score = $row["score"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($conn);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
 }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font Awesome -->
+    <link href="../../assets/fontawesome/css/all.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <!-- <link rel="icon" type="image/x-icon" href="assets/img/bell-regular.svg"> -->
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="../../assets/css/style.css?v=2.2">
+
+    <!-- Bootstrap Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Update Assignment - AMS</title>
+    <style>
+        .log-form {
+            margin-top: -25px;
+        }
+        .back-btn {
+        float: right; 
+        margin-right: 10px; 
+        margin-left: -70px; 
+        color: #5C5B5B; 
+        font-size: 12px; 
+        text-decoration: none;
+      }
+      .back-btn:hover {
+        color: #5C5B5B; 
+        text-decoration: none;
+      }
+    </style>
+</head>
+<body>
+
+<?php include(ROOT_PATH . "/includes/header.php"); ?>
+<?php //include(ROOT_PATH . "/includes/sidebar.php"); ?>
+
+<a class="back-btn me-3 me-lg-0"href="../../pages/courses.php"><i class="fas fa-arrow-left"></i> Back</a>
+<div class="d-flex justify-content-center">
+<form action="assignment-update.php" class="log-form" method="post">
+<?php //include('errors.php'); ?>
+
+<div class="form-header d-flex justify-content-center">
+    <div class="bg-circle">
+        <div class="sm-circle">
+        <div class="d-flex justify-content-center">
+        <i class="user-header fa-solid fa-clipboard fa-3x"></i>
+</div>
+        </div>
+    </div>
+</div>
+<br>
+<h2 class="text-center">Add Assignment</h2>
+
+
+   
+    <div class="d-flex justify-content-center">
+        <div class="form-group input-group w-75">
+    	    <div class="input-group-prepend">
+		        <span class="input-group-text"> <i class="fa fa-hashtag"></i> </span>
+		    </div>
+            <input name="score" class="form-control" placeholder="Score" type="number" value="<?php echo $score; ?>">
+        </div>
+    </div> <!-- form-group// -->
+
+
+
+
+  
+    <div class="d-flex justify-content-center">                                
+        <button type="submit" name="add-score" class="btn btn-primary text-center reg-log">Update score</button>  
+    </div>                                                             
+</form>
+    
+</body>
+</html>
