0efdac7631b29786f926b634dc1312c27fd6d26a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 08:27:58 -0600

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,7 +5,7 @@
                     <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>  Dashboard</span>
                   </a>
                   <a href="/pages/courses.php" class="list-group-item list-group-item-action py-2 ripple">
-                  <i class="fa-solid fa-landmark"></i><span>  Classes</span></a>
+                  <i class="fa-solid fa-landmark"></i><span>  Courses</span></a>
                   <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-clipboard-list"></i><span>  Assignments</span></a>
                   <a href="/pages/progress-report.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-bars-progress"></i><span>  Progress Report</span></a>
                 </div>
