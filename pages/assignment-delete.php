72a95dd14bce2c0153f3d2edbf71ae4ae5e56cf1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 11:03:56 -0600

    update

--- pages/assignment-delete.php
+++ pages/assignment-delete.php
@@ -2,8 +2,7 @@
 include("../database/connection.php");
 $sql = "DELETE FROM assignments WHERE id='" . $_GET["cid"] . "'";
 if (mysqli_query($conn, $sql)) {
-    $id = $_GET['cid'];
-    header('location: '. BASE_URL .'/pages/course-page.php?cid='. $id);
+    header('location: '. BASE_URL .'/pages/course-page.php?cid='. $_GET["cid"]);
     //echo "Record deleted successfully";
 } else {
     echo "Error deleting record: " . mysqli_error($conn);
