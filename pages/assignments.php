011900fcbd89ca0c16a9163c932b764faec1816d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 23:12:41 -0600

    update

--- pages/assignments.php
+++ pages/assignments.php
@@ -36,7 +36,6 @@ if(!isset($_SESSION['username'])){
 <div class="main">
 <!-- Assignments -->
 <br>
-    <a class="edit-btn me-3 me-lg-0" href="../action/assignments/add-assignment.php?cid=<?php echo $course_id; ?>"><i class="fas fa-pencil"></i> Add Assignment</a>
       <h3 class="page_title">Course Assignments</h3>
 
       <div class="col d-flex justify-content-center">
