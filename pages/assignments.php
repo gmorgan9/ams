670c439dfc958cc8d7f8d74fdd190dce22093f9a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 08:55:29 -0600

    update

--- pages/assignments.php
+++ pages/assignments.php
@@ -100,18 +100,18 @@ if(!isset($_SESSION['username'])){
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         ...
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Understood</button>
       </div>
     </div>
@@ -125,7 +125,7 @@ if(!isset($_SESSION['username'])){
 
 <script>
 var exampleModal = document.getElementById('myModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
+exampleModal.addEventListener('show.modal', function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget
 
