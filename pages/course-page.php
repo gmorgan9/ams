8c6031590ded95e38bf4be50abb4e768846057a5
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 17:19:15 -0600

    update

--- pages/course-page.php
+++ pages/course-page.php
@@ -210,7 +210,7 @@ if(!isset($_SESSION['username'])){
             } else { ?>
             <td><?php echo $ascore; ?></td>
             <?php } ?>
-            <td><a data-toggle="modal" data-target="#scoreModal" class="edit-btn me-3 me-lg-0" href="/pages/assignment-update.php?uid=<?php echo $aid; ?>"><i class="fas fa-pencil"></i></a>
+            <td><a data-toggle="modal" data-target="#scoreModal" class="edit-btn me-3 me-lg-0" href="/pages/course-page.php?uid=<?php echo $aid; ?>"><i class="fas fa-pencil"></i></a>
           </td>
             <td><a href="/pages/assignment-delete.php?did=<?php echo $aid; ?>" class="delete"><i class="fa-solid fa-trash-can" style="color:#941515;"></i></a></td>
             </tr>
