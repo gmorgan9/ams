63e726ea929e8b7a60433030de8571f4d6616eb3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 17:23:58 -0600

    update

--- pages/assignment-update.php
+++ pages/assignment-update.php
@@ -1,4 +1,5 @@
 <?php 
+if(isset($_POST['add-score'])) {
 $aid = $_GET['uid'];
 $query = "SELECT score FROM assignments WHERE id='$aid'";
 $result = mysqli_query( $conn, $query );
@@ -7,5 +8,5 @@ $row = mysqli_fetch_assoc( $query );
 $newhp = $row['score'];
 $query = "UPDATE assignments SET score='$newscore' WHERE id='$aid'";
 $result = mysqli_query( $conn, $query );
-
+}
 ?>
