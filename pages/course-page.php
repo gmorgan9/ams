5516eee54b3fb650cad54785fe5e70a68d0620e6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 09:53:16 -0600

    update

--- pages/course-page.php
+++ pages/course-page.php
@@ -251,7 +251,7 @@ if(!isset($_SESSION['username'])){
               	    <div class="input-group-prepend">
 		                  <span class="input-group-text"> <i class="fa fa-hashtag"></i> </span>
 		                </div>
-                    <input name="score" class="form-control" placeholder="Score" type="number" value="0">
+                    <input name="score" class="form-control" placeholder="Score" type="number">
                   </div>
               </div> <!-- form-group// -->
               <div class="d-flex justify-content-center">
