0daf9fe5e17ba8692c17923c71928c2ffbb3b169
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 23:23:27 -0600

    update

--- pages/assignments.php
+++ pages/assignments.php
@@ -28,6 +28,11 @@ if(!isset($_SESSION['username'])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Assignments - AMS</title>
+    <style>
+        .header-line {
+        border-bottom: 3px rgb(128, 128, 128) solid;
+      }
+    </style>
 </head>
 <body>
 
