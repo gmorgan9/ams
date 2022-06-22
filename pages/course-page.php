05166d98f3cadd449d110a399bb3df5a053470f7
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 17:22:44 -0600

    update

--- pages/course-page.php
+++ pages/course-page.php
@@ -227,7 +227,17 @@ if(!isset($_SESSION['username'])){
 <!-- Button trigger modal -->
 
 
+<?php 
+$aid = $_GET['uid'];
+$query = "SELECT score FROM assignments WHERE id='$aid'";
+$result = mysqli_query( $conn, $query );
+$row = mysqli_fetch_assoc( $query );
 
+$newhp = $row['score'];
+$query = "UPDATE assignments SET score='$newscore' WHERE id='$aid'";
+$result = mysqli_query( $conn, $query );
+
+?>
 
 
 <!-- ADD Modal -->
