072de22528d6ad1c1112527463ada8a7b3a2875a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-22 08:48:51 -0600

    update

--- pages/assignments.php
+++ pages/assignments.php
@@ -132,22 +132,7 @@ if(!isset($_SESSION['username'])){
 
 
 <script>
-var exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
-})
+var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
 </script>
 </body>
 </html>
