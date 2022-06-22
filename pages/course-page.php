c1f21638c22d029278e4845ba054a07c28601a6d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-21 19:47:36 -0600

    update

--- pages/course-page.php
+++ pages/course-page.php
@@ -96,13 +96,15 @@ if(!isset($_SESSION['username'])){
                     <p class="card-text text-center"><b>Course Location:</b> <?php echo $location; ?></p>
                 </div>
                 <div class="col">
-                    <h3 class="card-title text-center">Lab Info</h3>
+                    <h4 class="card-title text-center">Lab Info</h4>
                     <p class="card-text text-center"><b>Lab Days:</b> <?php echo $lab_day; ?></p>
                     <p class="card-text text-center"><b>Lab Times:</b> <?php echo $lab_time; ?></p>
                     <p class="card-text text-center"><b>Lab Location:</b> <?php echo $lab_location; ?></p>
                 </div>
                 </div>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
     </div>
